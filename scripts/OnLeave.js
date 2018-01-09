7677f08cd59fbd8e74aa50952360075291193423
Vena <gauthiervenaruzzo@orange.fr>
2018-01-09 15:59:56 +0100

    Second Push

--- scripts/OnLeave.js
+++
@@ -1,9 +0,0 @@
-function onLeave(anchorLink, index, slideIndex, direction){
-    var leavingSection = $(this);
-    anchorLink = 'anchor_truckaccess';
-     if (index == 2 && slideIndex == 0 && direction == 'down') {
-//            $('#fullpage').fullpage.moveSlideRight();
-        }
-}
-
-
